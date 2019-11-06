321310af914f6aaaf851c2271e0906c70c171583
Jesse Shaffer <dajester2005@gmail.com>
2019-11-06 10:49:37 -0600

    cannot find java in "c:\program files"
    java not found because !JAVA_IN_PATH!

--- stork-launcher/src/main/resources/com/fizzed/stork/launcher/windows/batch-find-java.bat
+++ stork-launcher/src/main/resources/com/fizzed/stork/launcher/windows/batch-find-java.bat
@@ -40,7 +40,7 @@ if NOT "%JAVA_HOME%"=="" (
 call :JavaSearchDebug "Searching PATH..."
 for %%X in (java.exe) do (set JAVA_IN_PATH=%%~$PATH:X)
 IF DEFINED JAVA_IN_PATH (
-    call :IsJavaBinVersionAcceptable !JAVA_IN_PATH! !target_java_ver_num! java_bin_accepted
+    call :IsJavaBinVersionAcceptable "!JAVA_IN_PATH!" !target_java_ver_num! java_bin_accepted
     if NOT "!java_bin_accepted!" == "" goto :AcceptableJavaBinFound
 )
 
